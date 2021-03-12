0ec7c68a6881ce3a140265cd96a52a2669bc8d6c
Michael Schams <typo3@2020.schams.net>
2021-03-12 22:26:29 +1100

    [TASK] Update TYPO3 Core dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -31,7 +31,7 @@ $EM_CONF[$_EXTKEY] = [
     ],
     'constraints' => [
         'depends' => [
-            'typo3' => '11.0.0-11.0.99',
+            'typo3' => '11.0.0-11.1.99',
             'php' => '7.4.0-7.4.99'
         ],
         'conflicts' => [],
