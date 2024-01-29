5eb934d39a0a96f35dc09c4d3d80234babb695de
Michael Schams <noreply@example.com>
2024-01-29 14:39:17 +1100

    [TASK] Bump version to v13.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,11 +20,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'schams.net',
     'state' => 'beta',
     'clearCacheOnLoad' => true,
-    'version' => '12.0.0',
+    'version' => '13.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.0.0-12.99.99',
-            'php' => '8.1.0-8.1.99'
+            'typo3' => '13.0.0-13.4.99'
         ],
         'conflicts' => [],
         'suggests' => []
